c582baf18f6827a03d823262203266f9f43e2942
thechitransh <thechitranshsrivastava@gmail.com>
2025-01-07 00:48:56 +0530

    add db

--- Db/db.js
+++ Db/db.js
@@ -1,9 +1,14 @@
 const moongoose = require("mongoose");
 
-const db = moongoose.connect("mongodb://localhost:27017/store").then(() => {
+const db = moongoose
+  .connect(
+    `mongodb+srv://chitranshnow:gS7HCnByA3SpWw9M@shopapp.gqp1k.mongodb.net/?retryWrites=true&w=majority&appName=shopapp`
+  )
+  .then(() => {
     console.log("connection successful");
-}).catch((e) => {
+  })
+  .catch((e) => {
     console.log("connection failed");
-});
+  });
 
 module.exports = db;
